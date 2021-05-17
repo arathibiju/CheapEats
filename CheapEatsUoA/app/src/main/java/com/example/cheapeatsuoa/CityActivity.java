45c8d35f38fc62cab67c0c527ef3ba3ff92cc922
Arathi Biju <abij996@aucklanduni.ac.nz>
2021-05-17 16:14:37 +1200

    added empty page for city activity

---
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/CityActivity.java
@@ -0,0 +1,22 @@
+package com.example.cheapeatsuoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CityActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+
+
+    }
+
+
+}
--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/MainActivity.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/MainActivity.java
@@ -1,15 +1,41 @@
 package com.example.cheapeatsuoa;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    class ViewHolder{
+        CardView cityCardView;
+
+        public ViewHolder() {
+            cityCardView =  findViewById(R.id.card_view_city);
+        }
+    }
+
+
+    ViewHolder vh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vh = new ViewHolder();
+        vh.cityCardView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent cityCampusActivity = new Intent(getBaseContext(),CityActivity.class);
+                cityCampusActivity.putExtra("FromMainActivity", "I'm from the MainActivity"); // sending object is more proffessional way byt then we need to add more code t change class to serializable or parseable
+                startActivity(cityCampusActivity);
+            }
+        });
+
     }
 }
