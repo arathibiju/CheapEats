85823aefbb25aab084e7fad6c04c263b31cf8b30
Shreya Singaraju <ssin733@aucklanduni.ac.nz>
2021-05-21 15:10:06 +1200

    Implemented simple list view for grafton and off campus activity

--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/GraftonActivity.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/GraftonActivity.java
@@ -3,12 +3,38 @@ package com.example.cheapeatsuoa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.cheapeatsuoa.Data.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class GraftonActivity extends AppCompatActivity {
+    class ViewHolder{
+        ListView listViewStores;
+
+        public ViewHolder(){
+            listViewStores = findViewById(R.id.list_view_grafton);
+        }
+    }
+    GraftonActivity.ViewHolder vh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafton);
+
+        vh = new GraftonActivity.ViewHolder();
+
+        Map<Integer, String> wordsDict = DataProvider.generateGraftonStoreNames();
+        List<String> stores = new ArrayList<String>( wordsDict.values());
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                stores);
+        ListView listView = (ListView) findViewById(R.id.list_view_grafton);
+        listView.setAdapter(itemsAdapter);
     }
 }
--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/OffCampusActivity.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/OffCampusActivity.java
@@ -3,12 +3,38 @@ package com.example.cheapeatsuoa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.cheapeatsuoa.Data.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class OffCampusActivity extends AppCompatActivity {
+    class ViewHolder{
+        ListView listViewStores;
+
+        public ViewHolder(){
+            listViewStores = findViewById(R.id.list_view_off_campus);
+        }
+    }
+    OffCampusActivity.ViewHolder vh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_off_campus);
+
+        vh = new OffCampusActivity.ViewHolder();
+
+        Map<Integer, String> wordsDict = DataProvider.generateOffCampusStoreNames();
+        List<String> stores = new ArrayList<String>( wordsDict.values());
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                stores);
+        ListView listView = (ListView) findViewById(R.id.list_view_off_campus);
+        listView.setAdapter(itemsAdapter);
     }
 }
