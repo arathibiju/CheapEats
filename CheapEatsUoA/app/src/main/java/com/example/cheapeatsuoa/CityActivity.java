f4e77a9be59905fba6fba69711cb1d9ac94e749f
Shreya Singaraju <ssin733@aucklanduni.ac.nz>
2021-05-31 16:31:52 +1200

    Created an adapter and implemented recycler view
    Need to connect recycler view to the ArrayList<Stores> from Dataprovider

--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/CityActivity.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/CityActivity.java
@@ -1,8 +1,11 @@
 package com.example.cheapeatsuoa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,38 +24,62 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
-public class CityActivity extends AppCompatActivity {
+public class CityActivity extends AppCompatActivity{
+
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerViewAdapter recyclerViewAdapter;
+
+    int[] arr = {R.drawable.city_1, R.drawable.city_2,
+            R.drawable.city_3, R.drawable.city_4,
+            R.drawable.city_5, R.drawable.city_6,
+            R.drawable.city_7, R.drawable.city_8,
+            R.drawable.city_9, R.drawable.city_10};
+
+/*
     class ViewHolder{
         //example of how to use the view holder class
         GridView gridViewStores; // create a view type
 
         public ViewHolder(){
-            gridViewStores = findViewById(R.id.grid_view_city); // instantiate it using id
+            //gridViewStores = findViewById(R.id.grid_view_city); // instantiate it using id
         }
     }
     ViewHolder vh;
+*/
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
-        vh = new ViewHolder();
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager =  new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerViewAdapter =  new RecyclerViewAdapter(arr);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setHasFixedSize(true);
+    }
+
+
+/*    @Override
+    public void onItemClick(View view, int position) {
+        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
+    }*/
+
 
         // then we can use the view by going vh.gridViewStores
-        GridView gridView = findViewById(R.id.grid_view_city);
+        //GridView gridView = findViewById(R.id.grid_view_city);
 
-        Intent receiveIntent = getIntent();
-        ArrayList<Store> cityStores = receiveIntent.getParcelableArrayListExtra("City");
-        /* we can then use cityStores list to populate the recycler view first. like maybe use a
+        //Intent receiveIntent = getIntent();
+        //ArrayList<Store> cityStores = receiveIntent.getParcelableArrayListExtra("City");
+
+
+    /* we can then use cityStores list to populate the recycler view first. like maybe use a
          loop to go through the list then do the getStoreName method for each to get the list of names.
          At least i think this is how it is. feel free to make any changes btw, idk if any of this is
          like good coding practice*/
-
-
-
-
-
 
 
 
@@ -68,8 +95,5 @@ public class CityActivity extends AppCompatActivity {
                 stores);
             ListView listView = (ListView) findViewById(R.id.list_view_city);
             listView.setAdapter(itemsAdapter);*/
-
-    }
-
 
 }
--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/RecyclerViewAdapter.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/RecyclerViewAdapter.java
@@ -1,7 +1,59 @@
 package com.example.cheapeatsuoa;
 
-import android.widget.ArrayAdapter;
-/*
-public class RecyclerViewAdapter extends ArrayAdapter{
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-}*/
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cheapeatsuoa.Model.Store;
+
+import java.util.ArrayList;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.myViewHolder> {
+
+    //ArrayList<Store> arr;
+    int [] arr2;
+
+    public RecyclerViewAdapter(int [] arr2) {
+        //this.arr = arr;
+        this.arr2 = arr2;
+    }
+
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view,parent,false);
+        myViewHolder myViewHolder = new myViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+    holder.imageView.setImageResource(arr2[position]);
+    holder.textView.setText("store no." + (position+1));
+    }
+
+    @Override
+    public int getItemCount() {
+        return arr2.length;
+    }
+
+    public class myViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textView;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.imageView);
+            textView =itemView.findViewById(R.id.textView);
+        }
+    }
+}
+
