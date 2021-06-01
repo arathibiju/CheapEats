5b8d4836da54db8a23180227f1393fce2da8b7ab
Shreya Singaraju <ssin733@aucklanduni.ac.nz>
2021-06-01 17:35:47 +1200

    Updated to cardView + new theme/style, added to all 3 categories
    Need to implement onClick and connect images to Details Activity

--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/CityActivity.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/CityActivity.java
@@ -34,55 +34,32 @@ public class CityActivity extends AppCompatActivity{
     //ArrayList<Store> cityStores = receiveIntent.getParcelableArrayListExtra("City");
     ArrayList<Store> cityStores = DataProvider.getCityStores();
 
-
     /* we can then use cityStores list to populate the recycler view first. like maybe use a
          loop to go through the list then do the getStoreName method for each to get the list of names.
          At least i think this is how it is. feel free to make any changes btw, idk if any of this is
          like good coding practice*/
 
-    int[] arr = {R.drawable.city_1, R.drawable.city_2,
-            R.drawable.city_3, R.drawable.city_4,
-            R.drawable.city_5, R.drawable.city_6,
-            R.drawable.city_7, R.drawable.city_8,
-            R.drawable.city_9, R.drawable.city_10};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
-        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.city_recycler_view);
         layoutManager =  new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
         recyclerViewAdapter =  new RecyclerViewAdapter(this, R.layout.activity_city, cityStores);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setHasFixedSize(true);
     }
-
-
-/*    @Override
-    public void onItemClick(View view, int position) {
-        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
-    }*/
-
-
-        // then we can use the view by going vh.gridViewStores
-        //GridView gridView = findViewById(R.id.grid_view_city);
-
-
-
-
-
-        // Instance of ImageAdapter Class
-       // gridView.setAdapter(new Store(this));
-
-        //
-
-/*        Map<Integer, String>  wordsDict = DataProvider.generateCityStoreNames();
-        List<String> stores = new ArrayList<>( wordsDict.values());
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                stores);
-            ListView listView = (ListView) findViewById(R.id.list_view_city);
-            listView.setAdapter(itemsAdapter);*/
-
 }
+
+
+/*    int[] arr = {R.drawable.city_1, R.drawable.city_2,
+            R.drawable.city_3, R.drawable.city_4,
+            R.drawable.city_5, R.drawable.city_6,
+            R.drawable.city_7, R.drawable.city_8,
+            R.drawable.city_9, R.drawable.city_10};*/
+
+
+
+
+
--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/GraftonActivity.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/GraftonActivity.java
@@ -1,6 +1,8 @@
 package com.example.cheapeatsuoa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,7 +17,27 @@ import java.util.List;
 import java.util.Map;
 
 public class GraftonActivity extends AppCompatActivity {
-    class ViewHolder{
+
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerViewAdapter recyclerViewAdapter;
+    ArrayList<Store> graftonStores = DataProvider.getGraftonStores();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grafton);
+        recyclerView = findViewById(R.id.grafton_recycler_view);
+        layoutManager =  new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerViewAdapter =  new RecyclerViewAdapter(this, R.layout.activity_grafton, graftonStores);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setHasFixedSize(true);
+    }
+
+
+
+/*     class ViewHolder{
         ListView listViewStores;
 
         public ViewHolder(){
@@ -41,5 +63,5 @@ public class GraftonActivity extends AppCompatActivity {
                 stores);
         ListView listView = (ListView) findViewById(R.id.list_view_grafton);
         listView.setAdapter(itemsAdapter);
-    }
+    }*/
 }
