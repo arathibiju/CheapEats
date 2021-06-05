497b2dbea1243f46d05b6f502affd3534e937347
Arathi Biju <abij996@aucklanduni.ac.nz>
2021-06-05 14:23:07 +1200

    Added logo to splash screen

--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/SplashScreen.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/SplashScreen.java
@@ -2,15 +2,27 @@ package com.example.cheapeatsuoa;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
