f82f3f3408ca00c3b1317a5bde0fe85c0cc5d71a
Shreya Singaraju <ssin733@aucklanduni.ac.nz>
2021-06-05 12:39:08 +1200

    Fixed Top Picks - checking for items that are already in top picks array list

--- CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/RecyclerViewAdapter.java
+++ CheapEatsUoA/app/src/main/java/com/example/cheapeatsuoa/RecyclerViewAdapter.java
@@ -5,21 +5,16 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.cheapeatsuoa.Data.DataProvider;
 import com.example.cheapeatsuoa.Model.Store;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.myViewHolder> {
@@ -41,22 +36,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     }
 
-    /*public static ArrayList<Store> getRecentStores(Context c) {
-            ArrayList<Store> storeList = new ArrayList<Store>();
-            storeList.add(c.get(position));
-            return storeList;
-    }*/
-
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view,parent,false);
         myViewHolder myViewHolder = new myViewHolder(view);
 
-
-
         return myViewHolder;
-
     }
 
     @Override
@@ -67,16 +53,38 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         holder.imageView.setImageResource(i);
         holder.textView.setText(mStores.get(position).getStoreName());
-        //lastOnClickStore = new Store (mStores.get(position).getIndex(), mStores.get(position).getImage(),mStores.get(position).getImage_b(),mStores.get(position).getImage_c(),mStores.get(position).getStoreName(),mStores.get(position).getLocation());
 
+        //Text onClick//
+        holder.textView.setOnClickListener(myViewHolder -> {
+            Intent detailActivity = new Intent(mContext,StoreDetailActivity.class);
+            detailActivity.putExtra("FromActivity", mStores.get(position));
+            mContext.startActivity(detailActivity);
 
-        holder.textView.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View myViewHolder){
-                Intent detailActivity = new Intent(mContext,StoreDetailActivity.class);
-                detailActivity.putExtra("FromActivity", mStores.get(position));
-                mContext.startActivity(detailActivity);
+            //Checks if most recent store clicked is already part of 'Recent Views'//
+            //Currently compares store names which works for our data, in the future if there multiple stores with the same name this might be an issue
+            if ((mStores.get(position).getStoreName()).equals(lastOnClickStore1.getStoreName())){
+                lastOnClickStore3 = lastOnClickStore3;
+                lastOnClickStore3.setDescription(lastOnClickStore3.getDescription());
+                lastOnClickStore3.setCost(lastOnClickStore3.getCost());
+                lastOnClickStore2 = lastOnClickStore2;
+                lastOnClickStore2.setDescription(lastOnClickStore2.getDescription());
+                lastOnClickStore2.setCost(lastOnClickStore2.getCost());
+                lastOnClickStore1 = lastOnClickStore1;
+                lastOnClickStore1.setDescription(lastOnClickStore1.getDescription());
+                lastOnClickStore1.setCost(lastOnClickStore1.getCost());
+            }
+            else if ((mStores.get(position).getStoreName()).equals(lastOnClickStore2.getStoreName())){
+                lastOnClickStore3 = lastOnClickStore3;
+                lastOnClickStore3.setDescription(lastOnClickStore3.getDescription());
+                lastOnClickStore3.setCost(lastOnClickStore3.getCost());
+                lastOnClickStore2 = lastOnClickStore1;
+                lastOnClickStore2.setDescription(lastOnClickStore1.getDescription());
+                lastOnClickStore2.setCost(lastOnClickStore1.getCost());
+                lastOnClickStore1 = new Store (mStores.get(position).getIndex(), mStores.get(position).getImage(),mStores.get(position).getImage_b(),mStores.get(position).getImage_c(),mStores.get(position).getStoreName(),mStores.get(position).getLocation());
+                lastOnClickStore1.setDescription(mStores.get(position).getDescription());
+                lastOnClickStore1.setCost(mStores.get(position).getCost());
+            }
+            else {
                 lastOnClickStore3 = lastOnClickStore2;
                 lastOnClickStore3.setDescription(lastOnClickStore2.getDescription());
                 lastOnClickStore3.setCost(lastOnClickStore2.getCost());
@@ -89,20 +97,38 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             }
         });
 
-        holder.imageView.setOnClickListener(new View.OnClickListener(){
+        //Image onClick//
+        holder.imageView.setOnClickListener(myViewHolder -> {
+            Intent detailActivity = new Intent(mContext,StoreDetailActivity.class);
 
+            detailActivity.putExtra("FromActivity", mStores.get(position)); // sending object is more proffessional way byt then we need to add more code t change class to serializable or parseable
+            mContext.startActivity(detailActivity);
 
-            @Override
-            public void onClick(View myViewHolder){
-                Intent detailActivity = new Intent(mContext,StoreDetailActivity.class);
-
-                detailActivity.putExtra("FromActivity", mStores.get(position)); // sending object is more proffessional way byt then we need to add more code t change class to serializable or parseable
-                mContext.startActivity(detailActivity);
-                //(0, "city_1", "city_1b", "city_1c", "Mojo", "HSB Courtyard, Auckland University, 10 Symonds Street");
-                //lastOnClickStore = new Store (mStores.get(position).getIndex(), mStores.get(position).getImage(),mStores.get(position).getImage_b(),mStores.get(position).getImage_c(),mStores.get(position).getStoreName(),mStores.get(position).getLocation());
-                //DataProvider.getRecentStores(mStores.get(position));
-/*                ArrayList<Store> storeList = new ArrayList<Store>(3);
-                storeList.add(mStores.get(position));*/
+            //Checks if most recent store clicked is already part of 'Recent Views'//
+            //Currently compares store names which works for our data, in the future if there multiple stores with the same name this might be an issue
+            if ((mStores.get(position).getStoreName()).equals(lastOnClickStore1.getStoreName())){
+                lastOnClickStore3 = lastOnClickStore3;
+                lastOnClickStore3.setDescription(lastOnClickStore3.getDescription());
+                lastOnClickStore3.setCost(lastOnClickStore3.getCost());
+                lastOnClickStore2 = lastOnClickStore2;
+                lastOnClickStore2.setDescription(lastOnClickStore2.getDescription());
+                lastOnClickStore2.setCost(lastOnClickStore2.getCost());
+                lastOnClickStore1 = lastOnClickStore1;
+                lastOnClickStore1.setDescription(lastOnClickStore1.getDescription());
+                lastOnClickStore1.setCost(lastOnClickStore1.getCost());
+            }
+            else if ((mStores.get(position).getStoreName()).equals(lastOnClickStore2.getStoreName())){
+                lastOnClickStore3 = lastOnClickStore3;
+                lastOnClickStore3.setDescription(lastOnClickStore3.getDescription());
+                lastOnClickStore3.setCost(lastOnClickStore3.getCost());
+                lastOnClickStore2 = lastOnClickStore1;
+                lastOnClickStore2.setDescription(lastOnClickStore1.getDescription());
+                lastOnClickStore2.setCost(lastOnClickStore1.getCost());
+                lastOnClickStore1 = new Store (mStores.get(position).getIndex(), mStores.get(position).getImage(),mStores.get(position).getImage_b(),mStores.get(position).getImage_c(),mStores.get(position).getStoreName(),mStores.get(position).getLocation());
+                lastOnClickStore1.setDescription(mStores.get(position).getDescription());
+                lastOnClickStore1.setCost(mStores.get(position).getCost());
+            }
+            else {
                 lastOnClickStore3 = lastOnClickStore2;
                 lastOnClickStore3.setDescription(lastOnClickStore2.getDescription());
                 lastOnClickStore3.setCost(lastOnClickStore2.getCost());
@@ -113,7 +139,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 lastOnClickStore1.setDescription(mStores.get(position).getDescription());
                 lastOnClickStore1.setCost(mStores.get(position).getCost());
             }
-
         });
     }
 
